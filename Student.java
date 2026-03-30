636f42f01634345f8fc751a2ac85fed10fe7cf47
Nivetha <nivetha26901@gmail.com>
2026-03-30 14:53:38 +0530

    Implemented Reportable interface

--- Student.java
+++ Student.java
@@ -2,36 +2,104 @@ package student_grade_tracker;
 
 import java.util.ArrayList;
 
-class Student {
+class Student implements Reportable {
 	private String name;
 	private String rollNo;
-	
+
 	private ArrayList<Grade> grades;
-	
-	public Student(String name, String rollNo){
+
+	public Student(String name, String rollNo) {
 		this.name = name;
 		this.rollNo = rollNo;
 		this.grades = new ArrayList<>();
 	}
-	
-	/* (not necessary)
-	//Setter Method - Name
-	public void setName(String name) {
-		this.name = name;
-	}
-	*/
-	
-	//Getter Methods - Name, RollNo
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getRollNo() {
 		return rollNo;
 	}
-	
+
+	public ArrayList<Grade> getGrades() {
+		return grades;
+	}
+
 	public void addGrade(Grade grade) {
 		grades.add(grade);
 	}
 	
+	//Average
+	public float average() {
+		int sum = 0;
+		for(Grade gr :grades) {
+			sum += gr.getMark();
+		}
+		int avg = sum / grades.size();
+		return avg;
+	}
+	
+	//GradeLetter
+	private char gradeLetter() {
+		float avg = average();
+		if(avg >= 90) return 'O';
+		else if (avg >= 80) return 'A';
+		else if (avg >= 70) return 'B';
+		else if (avg >= 60) return 'C'; 
+		else if (avg >= 50) return 'D';
+		else return 'F';
+	}
+	
+	
+	public Grade highestGrade() {
+		Grade maxMark = grades.get(0);
+		for(Grade gr : grades) {
+			if(gr.getMark() > maxMark.getMark())
+				maxMark = gr;
+		}
+		return maxMark;
+	}
+	
+	public Grade lowestGrade() {
+		Grade lowMark = grades.get(0);
+		for(Grade gr : grades) {
+			if(gr.getMark() < lowMark.getMark())
+				lowMark = gr;
+		}
+		return lowMark;
+	}
+	
+	@Override
+	public void generateReport() {
+		System.out.println("=========================================");
+        System.out.println("  STUDENT REPORT");
+        System.out.println("=========================================");
+        System.out.println("  Name        : " + name);
+        System.out.println("  Roll Number : " + rollNo);
+        System.out.println("-----------------------------------------");
+        System.out.println("  Subject          Marks");
+        System.out.println("  ................................");
+        
+        if(grades.isEmpty()) {
+        	System.out.println(" (No grades recorded yet)");
+        }
+        else {
+        	for(Grade gr : grades) {
+        		System.out.printf("  %-16s %d\n", gr.getSubject(), gr.getMark());
+        	}
+        	System.out.println("  ................................");
+            System.out.printf("  Average        :  %.2f\n", average());
+        	
+        	Grade max = highestGrade();
+        	System.out.printf("  Highest        :  %s (%d)\n", max.getSubject(), max.getMark());
+        	
+        	Grade low = lowestGrade();
+        	System.out.printf("  Lowest         :  %s (%d)\n", low.getSubject(), low.getMark());
+            System.out.println("");
+            System.out.println("  Grade Letter   :  " + gradeLetter());
+        }
+        System.out.println("=========================================");
+	}
+
 }
